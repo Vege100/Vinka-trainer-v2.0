48f547319d992726c41907e29a7c8941af33a7f4
Kilpeläinen Verneri Valo Johannes <verneri.k.kilpelainen@student.jyu.fi>
2022-10-15 19:54:45 +0300

    melkee 50%

--- VinkaTrainer2/paaohjelma/paaohjelma.cs
+++ VinkaTrainer2/paaohjelma/paaohjelma.cs
@@ -11,47 +11,68 @@ namespace paaohjelma
     {
         PhysicsObject Pelaaja;
         double ohjusnopeus = -100;
+        PhysicsObject ohjus;
         public override void Begin()
+        {
+            Valikko();
+
+            Keyboard.Listen(Key.Escape, ButtonState.Pressed, Valikko,"kakka");
+        }
+        void Valikko()
+        {
+            MultiSelectWindow alkuValikko = new MultiSelectWindow("kakka", "Start", "Points", "Quit");
+            Add(alkuValikko);
+
+
+            alkuValikko.AddItemHandler(0, Start);
+            alkuValikko.AddItemHandler(1, Points);
+            alkuValikko.AddItemHandler(2, Exit);
+        }
+        void Start()
         {
             Luokentta();
             Asetaohjaimet();
-
             Timer.CreateAndStart(2, AmmuOhjus);
-            PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
-            Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+        }
+        void Points()
+        {
+            
         }
 
         void Luokentta()
         {
             Pelaaja = LuoPelaaja();
-            Level.CreateBorders();
-            Background tausta = new Background(1000, 1000);
+            Image tausta = LoadImage("ohjukset");
+            Level.Background.CreateStars(1000);
 
 
         }
         PhysicsObject LuoPelaaja()
         {
-            PhysicsObject Pelaaja = PhysicsObject.CreateStaticObject(100, 40);
+            PhysicsObject Pelaaja = PhysicsObject.CreateStaticObject(200,140);
             Pelaaja.Shape = Shape.Rectangle;
             Pelaaja.Y = 0;
-            Pelaaja.X = Level.Left + 20;
+            Pelaaja.X = Level.Left + 150;
             Pelaaja.Image = LoadImage("vinka1");
             Add(Pelaaja);
             return Pelaaja;
 
         }
+
         void AmmuOhjus()
         {
-            PhysicsObject ohjus = new PhysicsObject(100,20);
+            ohjus = new PhysicsObject(210,50);
             ohjus.Shape = Shape.Rectangle;
             int y = RandomGen.NextInt(-500, 500);
+
             ohjus.Y = y;
             ohjus.X = Level.Right;
-            string[] t = {"ohjus1", "ohjus2", "ohjus3", "ohjus4", "ohjus5", "ohjus6", "ohjus7" };
             int kuva = RandomGen.NextInt(0, 7);
+            string[] t = {"ohjus1", "ohjus2", "ohjus3", "ohjus4", "ohjus5", "ohjus6", "ohjus7" };
             ohjus.Image = LoadImage(t[kuva]);
+
             ohjus.Velocity = new Vector(ohjusnopeus, 0);
-            ohjusnopeus = ohjusnopeus*2;
+            ohjusnopeus = ohjusnopeus*1.1;
             Add(ohjus);
 
         }
