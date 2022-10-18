dd1d9dec599a486b11d9eddeb10a2f33e78635b3
Kilpeläinen Verneri Valo Johannes <verneri.k.kilpelainen@student.jyu.fi>
2022-10-18 16:36:49 +0300

    Graafista sunnittelua ja valikon aloittamista

--- VinkaTrainer2/paaohjelma/paaohjelma.cs
+++ VinkaTrainer2/paaohjelma/paaohjelma.cs
@@ -4,41 +4,78 @@ using Jypeli.Controls;
 using Jypeli.Widgets;
 using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace paaohjelma
 {
     public class paaohjelma : PhysicsGame
     {
         PhysicsObject Pelaaja;
-        double ohjusnopeus = -100;
-        PhysicsObject ohjus;
+        IntMeter pisteLaskuri;
+        int raha = 3;
+        int elama = 3;
         public override void Begin()
         {
             Valikko();
-
-            Keyboard.Listen(Key.Escape, ButtonState.Pressed, Valikko,"kakka");
         }
         void Valikko()
         {
-            MultiSelectWindow alkuValikko = new MultiSelectWindow("kakka", "Start", "Points", "Quit");
+            ClearAll();
+            MultiSelectWindow alkuValikko = new MultiSelectWindow("kakka", "Start", "Points", "Shop", "Quit");
             Add(alkuValikko);
 
 
             alkuValikko.AddItemHandler(0, Start);
             alkuValikko.AddItemHandler(1, Points);
-            alkuValikko.AddItemHandler(2, Exit);
+            alkuValikko.AddItemHandler(2, Shop);
+            alkuValikko.AddItemHandler(3, Exit);
+        }
+        void Pausevalikko()
+        {
+            MultiSelectWindow pausevalikko = new MultiSelectWindow("", "Continue", "Giveup");
+            Pause();
+            Add(pausevalikko);
+            pausevalikko.AddItemHandler(0, Continue);
+            pausevalikko.AddItemHandler(1, Giveup);
+
+        }
+        void Continue()
+        {
+            Pause();
+        }
+        void Giveup()
+        {
+            raha += pisteLaskuri.Value;
+            Pause();
+            Valikko();
         }
         void Start()
         {
             Luokentta();
             Asetaohjaimet();
+            LuoPistelaskuri();
             Timer.CreateAndStart(2, AmmuOhjus);
+
         }
         void Points()
         {
-            
+            MultiSelectWindow points = new MultiSelectWindow("", "Back");
+            Add(points);
+            points.AddItemHandler(0, Valikko);
         }
-
+        void Shop()
+        {
+            MultiSelectWindow shop = new MultiSelectWindow("", "Back", "Guns");
+            Add(shop);
+            shop.AddItemHandler(0, Valikko);
+            shop.AddItemHandler(1, Guns);
+            Label rahana = new Label(50,20,raha.ToString());
+            Add(rahana);
+        }
+        void Guns()
+        {
+            ClearAll();
+        }
         void Luokentta()
         {
             Pelaaja = LuoPelaaja();
@@ -49,11 +86,12 @@ namespace paaohjelma
         }
         PhysicsObject LuoPelaaja()
         {
-            PhysicsObject Pelaaja = PhysicsObject.CreateStaticObject(200,140);
-            Pelaaja.Shape = Shape.Rectangle;
+            Image vinka = LoadImage("vinkaframe3");
+            PhysicsObject Pelaaja = PhysicsObject.CreateStaticObject(501, 201);
+            Pelaaja.Shape = Shape.FromImage(vinka);
             Pelaaja.Y = 0;
             Pelaaja.X = Level.Left + 150;
-            Pelaaja.Image = LoadImage("vinka1");
+            Pelaaja.Color = Color.Red;
             Add(Pelaaja);
             return Pelaaja;
 
@@ -61,20 +99,35 @@ namespace paaohjelma
 
         void AmmuOhjus()
         {
+            PhysicsObject ohjus;
+            double ohjusnopeus = -100;
             ohjus = new PhysicsObject(210,50);
-            ohjus.Shape = Shape.Rectangle;
             int y = RandomGen.NextInt(-500, 500);
-
             ohjus.Y = y;
             ohjus.X = Level.Right;
             int kuva = RandomGen.NextInt(0, 7);
             string[] t = {"ohjus1", "ohjus2", "ohjus3", "ohjus4", "ohjus5", "ohjus6", "ohjus7" };
-            ohjus.Image = LoadImage(t[kuva]);
-
+            Image ohjuskuva = LoadImage(t[kuva]);
+            ohjus.Shape = Shape.FromImage(ohjuskuva);
+            ohjus.Image = ohjuskuva;
+            pisteLaskuri.Value += 1;
             ohjus.Velocity = new Vector(ohjusnopeus, 0);
             ohjusnopeus = ohjusnopeus*1.1;
             Add(ohjus);
 
+        }
+        void LuoPistelaskuri()
+        {
+            pisteLaskuri = new IntMeter(0);
+
+            Label pisteNaytto = new Label();
+            pisteNaytto.X = Screen.Left + 100;
+            pisteNaytto.Y = Screen.Top - 100;
+            pisteNaytto.TextColor = Color.Black;
+            pisteNaytto.Color = Color.White;
+
+            pisteNaytto.BindTo(pisteLaskuri);
+            Add(pisteNaytto);
         }
         void Asetaohjaimet()
         {
@@ -91,6 +144,8 @@ namespace paaohjelma
             Keyboard.Listen(Key.Up, ButtonState.Released, AsetaNopeus, null, Pelaaja, Vector.Zero);
             Keyboard.Listen(Key.Down, ButtonState.Down, AsetaNopeus, "Kaarto alas", Pelaaja, nopeusAlas);
             Keyboard.Listen(Key.Down, ButtonState.Released, AsetaNopeus, null, Pelaaja, Vector.Zero);
+
+            Keyboard.Listen(Key.M, ButtonState.Pressed, Pausevalikko, "kakka");
         }
 
         void AsetaNopeus(PhysicsObject maila, Vector nopeus)
